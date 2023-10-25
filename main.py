b0ff050299eb6a43be2c1f35be12b1ec8869cdb6
jworth7 <149012920+jworth7@users.noreply.github.com>
2023-10-25 16:52:04 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -19,10 +19,6 @@ def main():
             first_pass = input('Please enter your password to encode: ')
             stored_pass = encode(first_pass)
             print('Your password has been encoded and stored!\n')
-        elif selection == '2':
-            print('The encoded password is ' + stored_pass, end=', ')
-            original_pass = decode(stored_pass)
-            print('and the original password is ' + original_pass + '\n')
         else:
             break
 
