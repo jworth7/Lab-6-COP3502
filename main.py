5f4e714cba28c1381d74d1beb16fec69ee10a030
jworth7 <149012920+jworth7@users.noreply.github.com>
2023-10-25 16:53:12 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def encode(password):
         new_password = new_password + str(int(password[i])+3)
     return new_password
 
+def decode(password):
+    
+
 def menu():
     print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n')
     a = input('Please enter an option: ')
@@ -19,6 +22,10 @@ def main():
             first_pass = input('Please enter your password to encode: ')
             stored_pass = encode(first_pass)
             print('Your password has been encoded and stored!\n')
+        elif selection == '2':
+            print('The encoded password is ' + stored_pass, end=', ')
+            original_pass = decode(stored_pass)
+            print('and the original password is ' + original_pass + '\n')
         else:
             break
 
